6f1113a6c595d380bc5e8e44ebd14fda323630ca
Aditya <aditya.pandey@dripcapital.com>
2022-10-17 21:22:54 +0530

    init

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Aditya Pandey", age: 22)
+User.create!(name: "Aditya Pandey1", age: 23)
+User.create!(name: "Aditya Pandey2", age: 24)
+User.create!(name: "Aditya Pandey3", age: 25)
+User.create!(name: "Aditya Pandey4", age: 26)
+User.create!(name: "Aditya Pandey5", age: 27)
+User.create!(name: "Aditya Pandey6", age: 28)
+User.create!(name: "Aditya Pandey7", age: 29)
+User.create!(name: "Aditya Pandey8", age: 30)
+User.create!(name: "Aditya Pandey9", age: 31)
+User.create!(name: "Aditya Pandey10", age: 32)
+User.create!(name: "Aditya Pandey11", age: 33)
+User.create!(name: "Aditya Pandey12", age: 34)
+User.create!(name: "Aditya Pandey13", age: 35)
